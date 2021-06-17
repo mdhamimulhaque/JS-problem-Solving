5494bdeaf03bb1bd3162a04690d6544b854bf596
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-18 02:03:39 +0600

    Even-odd

---
+++ Even-odd.js
@@ -0,0 +1,43 @@
+// =============Even Number===========
+
+for(var i = 1; i <= 100; i++){
+    if(i % 2 != 0){
+        continue;
+    }
+    console.log(i);
+}
+
+
+//========== 2nd way=========
+
+// for(var i = 1; i <= 100; i++){
+    
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+    
+// }
+
+
+// =============Odd Number===========
+
+for(var i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        continue;
+    }
+    console.log(i)
+}
+
+
+
+//========== 2nd way=========
+
+// for(var i = 1; i<=100; i++){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
