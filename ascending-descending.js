513d124573a28c14dbbb1ce82f145a62ed39faf4
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-18 02:06:12 +0600

    ascending-descending

---
+++ ascending-descending.js
@@ -0,0 +1,13 @@
+
+var names = ["A","B","C","D","E","F","G","H"];
+var result = names.sort();
+console.log(result);
+var result1 = result.reverse();
+console.log(result1);
+
+// numbers
+ var numbers = [1,3,7,6,0,5,9,2,4,8];
+ var result2 = numbers.sort();
+ console.log(result2);
+ var result3 = numbers.reverse();
+ console.log(result3);
