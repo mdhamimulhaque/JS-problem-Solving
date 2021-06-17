a877d8dee1dd8d0ad4f1981460a7c13c147d0b4a
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-18 02:01:15 +0600

    randoms

---
+++ random.js
@@ -0,0 +1,10 @@
+
+var random = Math.random()*100;
+
+var result = Math.round(random);
+var result1 = Math.ceil(random);
+var result2 = Math.floor(random);
+
+console.log(result);
+console.log(result1);
+console.log(result2);
