28a8757334a92d13b36572fdfd43d0a153ed97d7
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-18 02:07:06 +0600

    array-sum

---
+++ array-sum.js
@@ -0,0 +1,20 @@
+var numbers = [10,20,30,40,50];
+var add = 0;
+for(var i = 0; i < numbers.length; i++){
+    var elements = numbers[i];
+    add = add + elements; 
+    console.log("This is practical show(add): ",elements, add);
+}
+
+console.log(add);
+
+// mul
+var numbers1 =[20,40,60,80];
+var mul = 1;
+for(var i = 0; i <numbers1.length; i++){
+    var looping = numbers1[i];
+    mul = mul * looping;
+    
+}
+console.log(mul);
+
