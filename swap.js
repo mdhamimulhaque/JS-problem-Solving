c1f07feb4c70c1237b585843e16f5de5a7d01858
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-18 21:39:27 +0600

    swap update

--- swap.js
+++ swap.js
@@ -1,22 +1,22 @@
 
 // =============1st way==============
-// var a = 20;
-// var b = 30;
-// console.log("Before swap : a =" + a,  "b = " + b);
+var a = 20;
+var b = 30;
+console.log("Before swap : a =" + a,  "b = " + b);
 
-// a = 30;
-// b = 20;
-// console.log("After swap : a =" + a,  "b = " + b);
+a = 30;
+b = 20;
+console.log("After swap : a =" + a,  "b = " + b);
 
 // ==========2nd way=============
-// var x = 50;
-// var y = 70;
-// console.log("before swap : x =" + x,  "y = " + y);
-// x = x + y;
-// y = x- y;
-// x = x - y;
+var x = 50;
+var y = 70;
+console.log("before swap : x =" + x,  "y = " + y);
+x = x + y;
+y = x- y;
+x = x - y;
 
-// console.log("After swap : x =" + x,  "y = " + y);
+console.log("After swap : x =" + x,  "y = " + y);
 
 // ===========3nd way(js way)=============
 var p = 40;
