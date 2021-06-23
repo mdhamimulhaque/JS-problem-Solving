b72513d33d194dfea0a6e9b60345621a4c0254f4
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-06-23 21:42:14 +0600

    reverseNumberArray

---
+++ reverseNumberArray.js
@@ -0,0 +1,8 @@
+var num =[11,22,33,44,55];
+var myNumber = [];
+for (let i = 0; i < num.length; i++) {
+     const element = num[i];
+     var result =  myNumber.unshift(element);     
+     console.log( myNumber);
+     
+}
