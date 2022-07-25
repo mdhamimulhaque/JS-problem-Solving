bebce0fb065d7065485a1fc761849cbf44d655f0
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-07-25 14:33:26 +0600

    remove first two letter from a string or word

---
+++ sLearner4.js
@@ -0,0 +1,9 @@
+// Write a function that takes a word and returns the new word without including the first two characters.
+
+const returnWithoutFstTwoLatter = (word) => {
+    let res = word.slice(2)
+    console.log(res)
+
+}
+
+returnWithoutFstTwoLatter('MdHamim-Himel')
