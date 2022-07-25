43bb7bec8011e856640c1b56dac9cb6930e927bb
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-07-25 14:19:41 +0600

    calculate triangle area

---
+++ slearner3.js
@@ -0,0 +1,10 @@
+// Write a function that takes the base and height of a triangle and return its area.
+
+
+const triangleCalculator = (base, height) => {
+
+    let trisngleArea = .5 * (base * height);
+    console.log(trisngleArea);
+}
+
+triangleCalculator(10, 10)
