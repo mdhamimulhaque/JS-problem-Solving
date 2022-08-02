e7648d10dff2739d516c3358c070dc4d22033fcc
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-02 15:06:09 +0600

    loop reverse way

---
+++ reversLoop.js
@@ -0,0 +1,14 @@
+//----------- with while loop
+// var num = 10;
+
+// while (num >= 1) {
+//     console.log(num);
+//     num--;
+// }
+
+//----------- with for loop
+
+var num1 = 20;
+for (var i = num1; i >= 1; i--) {
+    console.log(i)
+}
