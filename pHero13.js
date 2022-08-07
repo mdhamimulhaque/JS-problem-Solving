376cdf7b779846971379d9ee7e7e53a83778e4f1
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-07 11:49:51 +0600

    max-min value find from an array

--- pHero13.js
+++ pHero13.js
@@ -2,13 +2,40 @@
 // =============================================================================
 
 
-const maxMinFun = (x, y, z) => {
-    let max = Math.max(x, y, z);
-    let min = Math.min(x, y, z)
+// const maxMinFun = (x, y, z) => {
+//     let max = Math.max(x, y, z);
+//     let min = Math.min(x, y, z)
+
+//     let finalResult = `The max number is ${max} and The min number is ${min}`;
+//     return finalResult;
+// }
+
+// const res = maxMinFun(10, 20, 30);
+// console.log(res);
+
+
+const maxMinFun = (arr) => {
+    const arrLength = arr.length - 1;
+
+    let max = arr[0];
+    let min = arr[0];
+
+
+    for (let i = 0; i <= arrLength; i++) {
+        // --- max value
+        if (arr[i] >= max) {
+            max = arr[i];
+        }
+        // --- min value
+        if (arr[i] <= min) {
+            min = arr[i];
+        }
+    }
 
     let finalResult = `The max number is ${max} and The min number is ${min}`;
     return finalResult;
+
 }
 
-const res = maxMinFun(10, 20, 30);
+const res = maxMinFun([10, 20, 30, 2, 6, 4, 40, 50, 60, 20, 353, 43, 24, 22, 500]);
 console.log(res);
