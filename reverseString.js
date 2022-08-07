8fefe6c6e910bf3ca65ea88e02587cc00b2e09b9
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-07 13:05:41 +0600

    reverse string value

--- reverseString.js
+++ reverseString.js
@@ -1,15 +1,45 @@
-function reverseString(a){
-    reversresult = 0;
-    for(var i = 0; i < a.length; i++){
-        var rvs = a[i];
-        reversresult = rvs + reversresult;
+
+const reverseFunction = (str) => {
+    let strLength = str.length - 1;
+
+    let reverseString = '';
+    for (let i = strLength; i >= 0; i--) {
+        let elements = str[i];
+        reverseString = reverseString + elements;
     }
-    return reversresult;
+    return reverseString;
 }
 
 
 
 
-var para ="This is Md Hamimul Haque";
-var result = reverseString(para);
-console.log(result);
+const text = 'Md Hamimimul Haque';
+const result1 = reverseFunction(text);
+console.log(result1)
+
+
+
+
+
+
+
+
+
+
+
+// =====================================================
+// function reverseString(a) {
+//     reversresult = 0;
+//     for (var i = 0; i < a.length; i++) {
+//         var rvs = a[i];
+//         reversresult = rvs + reversresult;
+//     }
+//     return reversresult;
+// }
+
+
+
+
+// var para = "This is Md Hamimul Haque";
+// var result = reverseString(para);
+// console.log(result);
