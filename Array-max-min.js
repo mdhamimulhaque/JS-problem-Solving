53c8185ea428f2c6e6812f89129163a3c1ccf7f7
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-07 12:10:37 +0600

    max-min value calculate with function

--- Array-max-min.js
+++ Array-max-min.js
@@ -1,3 +1,4 @@
+// ---------> with function
 const maxMinFun = (arr) => {
     const arrLength = arr.length - 1;
 
@@ -28,7 +29,7 @@ console.log(res);
 
 //=======================================================================================
 
-
+// --------->  without Function
 
 var numbers = [10, 20, 70, 40, 50];
 
