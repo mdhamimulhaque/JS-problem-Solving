b3da17eae8514928b54cef689b888d66cb651f96
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-09 00:46:17 +0600

    print odd number using while loop

---
+++ pHero17.js
@@ -0,0 +1,14 @@
+//  print odd number using while loop
+// =========================================
+
+
+let number = 1;
+
+while (number <= 100) {
+    let num = number;
+
+    if (num % 2 !== 0) {
+        console.log(num)
+    }
+    number++
+}
