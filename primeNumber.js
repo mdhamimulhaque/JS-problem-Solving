201bd4af28ae40891ef0c3303ba89af6c9a6516c
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-08 21:04:07 +0600

    primeNumber

--- primeNumber.js
+++ primeNumber.js
@@ -1,15 +1,16 @@
-function primeNumber(n){
-    for(var i = 2; i < n; i++){
+function primeNumber(n) {
+  for (var i = 2; i < n; i++) {
 
-        if(n % i == 0){
-          return "It's not a prime number";
-        }else{
-          return "It's a prime number";
-        }
+    if (n % i == 0) {
+      return "It's not a prime number";
+    } else {
+      return "It's a prime number";
     }
+  }
 }
 
 
 var result = primeNumber(120);
+console.log(result);
 var result = primeNumber(27);
 console.log(result);
